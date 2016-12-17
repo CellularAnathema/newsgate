4353886752b0b5daff12f06daf21e492490cbfb7
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2016-12-16 21:47:19 -0800

    Complete articleFinder.

---
+++ server/aylien/aylienArticleFindController.js
@@ -0,0 +1,94 @@
+/*
+Aylien News API - related news
+
+For more information, see: https://newsapi.aylien.com/docs/endpoints/related_stories/nodejs
+*/
+
+////// convention is to take the res object and append the new data from the middleware then either send it to the next middleware.
+
+// user enters a url into the browser, then starts the extension.
+
+  // the extension will then pull the url from the open tab and then send a post request to the server including that url ====>NEXT()
+
+  // the server router will listen for the post request and upon receiving it will invoke the watsonController.getKeywords function ====>NEXT()
+
+  // getKeywords function will pull the keywords from the article and respond with those keywords. ====>NEXT()
+
+  // articleFinder will take the keywords response and pipe that into the aylienController, to get related articles.
+
+  // aylienController will then ====>NEXT() back to the router so the router can send the response.
+
+
+
+////////////////////////////////////////////////////////////
+var Promise = require('bluebird');
+var aylienNewsApi = require('aylien-news-api');
+var aylienNonpromise = new aylienNewsApi.DefaultApi();
+var aylienGoop = require('./aylien_api_key.js');
+
+
+// GOOP EXAMPLE: {
+//   application_id: 'YOUR_APP_ID',
+//   application_key: 'YOUR_APP_KEY'
+// }
+
+
+// Configure API key authorization: app_id
+var app_id = aylienNonpromise.apiClient.authentications['app_id'];
+app_id.apiKey = aylienGoop.application_id;
+
+
+// Configure API key authorization: app_key
+var app_key = aylienNonpromise.apiClient.authentications['app_key'];
+app_key.apiKey = aylienGoop.application_key;
+
+var aylienArticles = Promise.promisifyAll(aylienNonpromise);
+
+
+module.exports.findRelatedArticles = function(req, res, next) {
+
+  // var keyskeys = res.compoundContent['keywords']['keywords'][0].text && res.compoundContent['keywords']['keywords'][1].text && res.compoundContent['keywords']['keywords'][2].text && res.compoundContent['keywords']['keywords'][3].text && res.compoundContent['keywords']['keywords'][4].text;
+
+  // var topKeywords = JSON.stringify(keyskeys);
+
+  var opts = {
+    'text' : '"Hillary Clinton" AND "Donald Trump"',
+    'language': ['en'],
+    'publishedAtStart': 'NOW-1DAY',
+    // 'sentimentBodyPolarity': 'positive',
+    // 'sentimentBodyPolarity': 'negative',
+    '_return': ['id', 'title', 'links'],
+    'publishedAtEnd': 'NOW',
+    'categoriesTaxonomy': 'iab-qag',
+    'categoriesId': ['IAB17'],
+    'perPage': 5
+  };
+
+
+  // console.log('inside opts');
+
+  // var callback = function(error, data, response) {
+  //   if (error) {
+  //     console.error(error);
+  //   } else {
+  //     console.log(data);
+  //   }
+  // };
+  // aylienNonpromise.listStories(opts, callback);
+
+  if (opts) {
+    aylienArticles.listStoriesAsync(opts)
+    .then(relatedArticles => {
+      res.compoundContent = res.compoundContent || {};
+
+      res.compoundContent['related'] = relatedArticles;
+      console.log(relatedArticles.stories[0].links);
+      next();
+    });
+  }
+
+
+
+};
+
+
--- server/watson/watsonController.js
+++ server/watson/watsonController.js
@@ -9,13 +9,16 @@ var watsonKey = require('./watson_api_key.js');
 var alchemy_language = watson.alchemy_language({
   api_key: watsonKey.watsonKey
 });
+var alchemy_data_news = watson.alchemy_data_news({
+  api_key: watsonKey.watsonKey
+});
 
 
 module.exports.getEmotions = function(req, res, next) {
   // No error checking if first in chain; assumes url already set
   var params = {
     url: res.compoundContent.url
-  }
+  };
   alchemy_language.emotion(params, function (err, response) {
     if (err)
       console.log('error:', err);
@@ -28,36 +31,80 @@ module.exports.getEmotions = function(req, res, next) {
 };
 
 module.exports.getTitle = function(req, res, next) {
-  console.log('request body', req.body.url);
+  // console.log('request body', req.body.url);
   var parameters = {
     url: req.body.url
   };
-
   alchemy_language.title(parameters, function (err, response) {
     if (err) {
       console.log('error:', err);
     } else {
-      console.log(JSON.stringify(response, null, 2));
+      // console.log(JSON.stringify(response, null, 2));
       res.compoundContent = res.compoundContent || {};
       res.compoundContent['title'] = response;
+      // console.log(res.compoundContent, 'in get title');
       next();
     }
   });
 };
 
 module.exports.getKeywords = function(req, res, next) {
-	var parameters = {
-		url: req.body.url,
-		maxRetrieve: 12
-	}
+  var parameters = {
+    url: req.body.url,
+    maxRetrieve: 2
+  };
 
   alchemy_language.keywords(parameters, function (err, response) {
     if (err) {
       console.log('error:', err);
     } else {
-      res.compoundContent['keywords'] = response;
+      res.compoundContent = res.compoundContent || {};
+      res.compoundContent['keywords'] = response.keywords;
+      // console.log(res.compoundContent, 'inside get keywords!');
     }
     next();
-    console.log(JSON.stringify(response, null, 2));
+  });
+};
+
+module.exports.getEntities = function(req, res, next) {
+  var parameters = {
+    url: req.body.url,
+    maxRetrieve: 5,
+    // sourceText: 'cleaned_or_raw'
+  };
+
+  alchemy_language.entities(parameters, function (err, response) {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      res.compoundContent = res.compoundContent || {};
+      res.compoundContent['entities'] = response.entities;
+      // console.log(res.compoundContent['entities'], 'inside entities!');
+    }
+    next();
+  });
+};
+
+
+module.exports.getRelated = function(req, res, next) {
+  console.log('inside get related');
+  var params = {
+
+    start: 'now-1M',
+    end: 'now',
+    count: 5,
+    return: 'enriched.url.title'
+  };
+
+  alchemy_data_news.getNews(params, function (err, response) {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      console.log('inside getnews');
+      res.compoundContent = res.compoundContent || {};
+      res.compoundContent['relatedArticles'] = response;
+      console.log('gotpast res');
+    }
+    next();
   });
 };
